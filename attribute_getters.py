994b34a3a3adffce2dde7395103d0f8aad0f1373
crayZsaaron <seth.a.yoder@gmail.com>
2013-08-08 09:54:03 -0400

    Right click to open links in a new tab. Works fine, but it would be nice to have the ability to close a tab and to navigate backward and forward in the browser history.

--- attribute_getters.py
+++ attribute_getters.py
@@ -3,7 +3,6 @@
 def capitalize(string):
     words = string.split(' ')
     for i in range(len(words)):
-        print(words)
         if words[i] not in ['the', 'a', 'of'] or i == 0:
             words[i] = words[i].capitalize()
     
--- gui.py
+++ gui.py
@@ -15,7 +15,6 @@ class UI(object):
         self.everything = None
 
         #Main window creation
-        main_window.setObjectName("main_window")
         main_window.resize(800, 600)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -25,18 +24,14 @@ class UI(object):
 
         #Central widget that contains all other widgets
         self.centralwidget = QtGui.QWidget(main_window)
-        self.centralwidget.setObjectName("centralwidget") #I honestly have no idea what this means.
 
         #Layout setup
         self.grid_layout_2 = QtGui.QGridLayout(self.centralwidget)
-        self.grid_layout_2.setObjectName("gridLayout_2")
         self.grid_layout = QtGui.QGridLayout()
-        self.grid_layout.setObjectName("gridLayout")
 
         #Search bar
         self.search_bar = QtGui.QLineEdit(main_window)
         self.search_bar.setText("")
-        self.search_bar.setObjectName("lineEdit")
         self.grid_layout.addWidget(self.search_bar, 0, 0, 1, 1)
 
         #Tabs
@@ -48,7 +43,6 @@ class UI(object):
         self.tab_count = 0
         self.browsers = [] #An array of browsers. So we can use tabbed browsing.
         self.browsers.append(QtWebKit.QWebView(main_window))
-        self.browsers[0].setObjectName("textBrowser")
 
         self.grid_layout_2.addLayout(self.grid_layout, 0, 0, 1, 1)
         main_window.setCentralWidget(self.centralwidget)
@@ -56,24 +50,17 @@ class UI(object):
         #Menu bar
         self.menubar = QtGui.QMenuBar(main_window)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 634, 19))
-        self.menubar.setObjectName("menubar")
         self.menu_file = QtGui.QMenu(self.menubar)
-        self.menu_file.setObjectName("menuFile")
         self.menuAbout = QtGui.QMenu(self.menubar)
-        self.menuAbout.setObjectName("menuAbout")
         main_window.setMenuBar(self.menubar)
 
         self.statusbar = QtGui.QStatusBar(main_window)
-        self.statusbar.setObjectName("statusbar")
         main_window.setStatusBar(self.statusbar)
 
         #Actions
         self.action_help = QtGui.QAction(main_window)
-        self.action_help.setObjectName("actionHelp")
         self.action_load_xml = QtGui.QAction(main_window)
-        self.action_load_xml.setObjectName("actionLoad_XML")
         self.action_exit = QtGui.QAction(main_window)
-        self.action_exit.setObjectName("actionExit")
 
         #Menu items
         self.menu_file.addAction(self.action_load_xml)
@@ -82,14 +69,32 @@ class UI(object):
         self.menubar.addAction(self.menu_file.menuAction())
         self.menubar.addAction(self.menuAbout.menuAction())
 
-        self.on_new_tab_page_load('sp0000')
+        #Right click context menu
+        self.action_openinnewtab = QtGui.QAction(main_window)
 
-        self.retranslateUi(main_window)
+        self.open_in_new_tab('sp0000')
+
+        self.set_titles(main_window)
         self.connect_actions(main_window)
         QtCore.QMetaObject.connectSlotsByName(main_window)
     
-    def on_new_tab_page_load(self, page_link):
-        print(page_link)
+    def set_titles(self, main_window):
+        main_window.setWindowTitle("Dwarf Fortress Legends Reader")
+        self.menu_file.setTitle("File")
+        self.menuAbout.setTitle("About")
+        self.action_help.setText("Help")
+        self.action_load_xml.setText("Load XML...")
+        self.action_exit.setText("Exit")
+        self.action_openinnewtab.setText("Open in new tab")
+
+    def open_in_current_tab(self, page_link):
+        try:
+            page_link = page_link.toString()
+        except:
+            pass
+        self.tab_widget.currentWidget().setHtml(page_builders.dispatch_link(page_link, self.everything))
+
+    def open_in_new_tab(self, page_link):
         try:
             #If it is a URL object, convert to a string first.
             page_link = page_link.toString()
@@ -114,7 +119,7 @@ class UI(object):
         #This allows me to handle the links by myself.
         webview.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
         #Call this method anytime a link is clicked (for now...)
-        webview.linkClicked.connect(self.on_new_tab_page_load)
+        webview.linkClicked.connect(self.open_in_current_tab)
         #web view now emits signal PySide.QtGui.QWidget.customContextMenuRequested
         webview.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         webview.customContextMenuRequested.connect(self.context_menu_requested)
@@ -123,20 +128,24 @@ class UI(object):
     #the location of the mouse click. We then test the page to see what is at that click location.
     def context_menu_requested(self, pos):
         hit_test = self.tab_widget.currentWidget().page().mainFrame().hitTestContent(pos)
-        hit_url = hit_test.linkUrl()
-        print(hit_url.isEmpty())
+        self.hit_url = hit_test.linkUrl()
 
-    def retranslateUi(self, main_window):
-        main_window.setWindowTitle(QtGui.QApplication.translate("main_window", "main_window", None, QtGui.QApplication.UnicodeUTF8))
-        self.menu_file.setTitle(QtGui.QApplication.translate("main_window", "File", None, QtGui.QApplication.UnicodeUTF8))
-        self.menuAbout.setTitle(QtGui.QApplication.translate("main_window", "About", None, QtGui.QApplication.UnicodeUTF8))
-        self.action_help.setText(QtGui.QApplication.translate("main_window", "Help", None, QtGui.QApplication.UnicodeUTF8))
-        self.action_load_xml.setText(QtGui.QApplication.translate("main_window", "Load XML...", None, QtGui.QApplication.UnicodeUTF8))
-        self.action_exit.setText(QtGui.QApplication.translate("main_window", "Exit", None, QtGui.QApplication.UnicodeUTF8))
+        global_pos = self.tab_widget.currentWidget().mapToGlobal(pos)
+
+        self.right_click_menu = QtGui.QMenu(self.tab_widget.currentWidget())
+        if not self.hit_url.isEmpty():
+            self.right_click_menu.addAction(self.action_openinnewtab)
+        self.right_click_menu.popup(global_pos) 
+
+        print(self.hit_url.isEmpty())
 
     def connect_actions(self, main_window):
         QtCore.QObject.connect(self.action_exit, QtCore.SIGNAL('triggered()'), QtCore.QCoreApplication.instance().quit)
         QtCore.QObject.connect(self.action_load_xml, QtCore.SIGNAL('triggered()'), self.file_dialog)
+        QtCore.QObject.connect(self.action_openinnewtab, QtCore.SIGNAL('triggered()'), self.on_click_openinnewtab)
+
+    def on_click_openinnewtab(self):
+        self.open_in_new_tab(self.hit_url)
 
     def file_dialog(self):
         self.file_dialog = QtGui.QFileDialog()
@@ -146,7 +155,7 @@ class UI(object):
     def xml_loaded(self):
         selected = self.file_dialog.selectedFiles()[0]
         self.everything = load_dict(selected)
-        self.on_new_tab_page_load('hf6666')
+        self.open_in_new_tab('hf6666')
 
 app = QtGui.QApplication(sys.argv)
 wid = QtGui.QMainWindow()
