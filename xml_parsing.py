dcd799c222ac9e168bcb5bc084f8282fc162356c
PT Fort <dcoilx@gmail.com>
2013-08-22 20:48:09 -0400

    Second round of optimization

--- xml_parsing.py
+++ xml_parsing.py
@@ -7,7 +7,7 @@ import os
 import codecs
 import time
 
-PROFILE_MEMORY = True
+PROFILE_MEMORY = False
 PROFILE_TIME = True
 
 try:
@@ -82,11 +82,56 @@ def load_dict(filename):
     for _, element in parser:
             
         if element.tag in lower_level_tags:
-            if element.tag == 'historical_figure':
-                element_data = load_historical_figure(element)
-            else:
-                element_data = load_generic_element(element)
             
+            element_data = {}
+            attributes = element.getchildren()
+            
+            if element.tag == 'historical_figure':
+                element_data['events'] = []
+                element_data['hf_links'] = []
+                element_data['entity_links'] = []
+                
+                for attribute in element:
+                    
+                    #not loaded
+                    if attribute.tag in ['hf_skill', 'entity_former_position_link']:
+                        continue
+                        
+                    #These tags have tags nested within them, and there are multiple for each historical figure,
+                    #Here, we parse them separately and store their information in subdictionaries within lists.
+                    if attribute.tag in ['hf_link', 'entity_link']:
+                        children = attribute.getchildren()
+                        
+                        attribute_dict = {}
+                        attribute_dict['type'] = children[0].text
+                        attribute_dict['id'] = int(children[1].text)
+                        
+                        if attribute.tag == 'entity_link' and len(children) > 2:
+                            attribute_dict['strength'] = int(children[2].text)
+                            
+                        element_data[attribute.tag + 's'].append(attribute_dict)
+                    else: #other hf_fig attribute
+                    
+                        #attributes such as death year for hf_figs still alive
+                        if attribute.text == "-1":
+                            continue
+                            
+                        try:
+                            element_data[attribute.tag] = int(attribute.text)
+                        except Exception:
+                            element_data[attribute.tag] = attribute.text
+                    
+            else: #generic element
+                #Add the element attributes to a dictionary representing the element
+                for attribute in attributes:
+                    if attribute.text == "-1":
+                        continue
+                            
+                    try:
+                        element_data[attribute.tag] = int(attribute.text)
+                    except Exception:
+                        element_data[attribute.tag] = attribute.text
+                        
             temp_element_array.append(element_data)
             close_element(element)
             
@@ -97,9 +142,10 @@ def load_dict(filename):
             #'historical_figures' section has id=5764. From there, the indexes progress one-by-one. So we can
             #set 'historical_figures_offset' to 5764. So then, accessing an element is easy and efficient; we just need to do
             #everything[element_type][id - offset]
-            if len(temp_element_array) > 0 and 'id' in temp_element_array[0].keys(): #FIX
-                #print("Offset is: " + str(temp_element_array[0]['id']))
+            try:
                 everything[element.tag + "_offset"] = temp_element_array[0]['id']
+            except Exception:
+                pass
                 
             everything[element.tag] = temp_element_array
             temp_element_array = []
@@ -147,57 +193,6 @@ def close_element(element):
     element.clear()                 # clean up children
     while element.getprevious() is not None:
         del element.getparent()[0]  # clean up preceding siblings
-
-def load_generic_element(element):
-    generic_element = {}
-    generic_element['events'] = []
-    
-    attributes = element.getchildren()
-        
-    #Add the element attributes to a dictionary representing the element
-    for attribute in attributes:
-       try:
-            generic_element[attribute.tag] = int(attribute.text)
-       except Exception:
-            generic_element[attribute.tag] = attribute.text
-       
-    return generic_element
-
-def load_historical_figure(element):
-    hist_fig = {}
-    hist_fig['events'] = []
-    hist_fig['hf_links'] = []
-    hist_fig['entity_links'] = []
-    
-    attributes = element.getchildren()
-    for attribute in element:
-        #These tags have tags nested within them, and there are multiple for each historical figure,
-        #Here, we parse them separately and store their information in subdictionaries within lists.
-        if attribute.tag not in ['hf_link', 'entity_link', 'hf_skill', 'entity_former_position_link']:
-            try:
-                hist_fig[attribute.tag] = int(attribute.text)
-            except Exception:
-                hist_fig[attribute.tag] = attribute.text
-            
-        elif attribute.tag == 'hf_link':
-            hf_link_dict = {}
-            for link_info in attribute.getchildren():
-                try:
-                    hf_link_dict[link_info.tag] = int(link_info.text)
-                except ValueError:
-                    hf_link_dict[link_info.tag] = link_info.text
-            hist_fig['hf_links'].append(hf_link_dict)
-
-        elif attribute.tag == 'entity_link':
-            entity_link_dict = {}
-            for link_info in attribute.getchildren():
-                try:
-                    entity_link_dict[link_info.tag] = int(link_info.text)
-                except ValueError:
-                    entity_link_dict[link_info.tag] = link_info.text
-            hist_fig['entity_links'].append(entity_link_dict)
-        
-    return hist_fig
     
 def add_event_link_to_hf(hfid, event_id, everything):
     get_element(hfid, 'historical_figures', everything)['events'].append(event_id)
@@ -206,13 +201,15 @@ def parse_historical_events(everything):
     if not 'historical_events' in everything:
         return
     
+    hfid_set = ['hfid', 'slayer_hfid', 'group_hfid', 'group_1_hfid', 'group_2_hfid', 'woundee_hfid',
+                           'wounder_hfid', 'trickster_hfid', 'cover_hfid', 'hist_fig_id', 'target_hfid', 
+                           'snatcher_hfid', 'changee_hfid', 'changer_hfid', 'hist_figure_id', 'hfid_target']
+                           
     for event_data in everything['historical_events']:
         for key in event_data.keys():
-            if key in set(['hfid', 'slayer_hfid', 'group_hfid', 'group_1_hfid', 'group_2_hfid', 'woundee_hfid',
-                           'wounder_hfid', 'trickster_hfid', 'cover_hfid', 'hist_fig_id', 'target_hfid', 
-                           'snatcher_hfid', 'changee_hfid', 'changer_hfid', 'hist_figure_id', 'hfid_target',]):
-                if event_data[key] != -1:
-                    add_event_link_to_hf(event_data[key], event_data['id'], everything)
+            if key in hfid_set:
+                add_event_link_to_hf(event_data[key], event_data['id'], everything)
+                
     '''
     for var in range(5500, 6100):
         print(get_name(var, 'historical_figures', everything) + ' events:')
