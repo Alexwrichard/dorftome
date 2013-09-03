4fcbb7497179f3c0fe4dce3564ab3ad0627546a1
crayZsaaron <seth.a.yoder@gmail.com>
2013-09-03 00:38:14 -0400

    Revert "Merging search bar addition by PT-Fort."
    This reverts commit 79507d975a435a3d354f23fa3a6f108c5aaa41b9.

--- gui/SearchBar.py
+++ gui/SearchBar.py
@@ -1,3 +1,4 @@
+
 from PySide import QtCore, QtGui, QtWebKit
 from attribute_getters import *
 from link_creator import *
@@ -13,8 +14,7 @@ class SearchBar(QtGui.QLineEdit):
         self.worker = SearchBar_Worker()
         
         #create pool with num CPUs minus one
-        #self.pool_size = cpu_count() - 1
-        self.pool_size = cpu_count()
+        self.pool_size = cpu_count() - 1
         self.pool = Pool(self.pool_size)
         
         #set the fcn to open links in the main GUI
